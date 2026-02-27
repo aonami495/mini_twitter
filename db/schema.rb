662a2c56428c32801a1d66948f4aa194bee076e3
aonami495 <aonami0495@gmail.com>
2026-02-27 16:50:30 +0900

    Add Follow model with self-referential association

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_25_043249) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_25_044445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "image_url"
@@ -45,6 +54,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_25_043249) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "images", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
