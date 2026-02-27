a4d7bf4cae2ad92c1f0edd0209cdb2635b405483
aonami495 <aonami0495@gmail.com>
2026-02-28 08:45:21 +0900

    Add devise authentication to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
